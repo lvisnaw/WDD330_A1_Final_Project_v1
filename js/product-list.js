de38f63c96c8c67795c763db7efaaa2d9678c232
lvisnaw <lvisnaw@gmail.com>
2024-04-08 19:32:22 -0600

    Just cleaning up files.

--- js/product-list.js
+++ js/product-list.js
@@ -1,31 +1,6 @@
 import productList from "./productList.mjs";
 import { getParam, loadHeaderFooter } from "./utils.mjs";
-// import { getProductsByCategory } from "./externalServices.mjs";
 
 loadHeaderFooter();
 const category = getParam("category");
 productList(".product-list", category);
-
-// function productCardTemplate(product) {
-//   return `<li class="product-card">
-//     <a href="/product_pages/index.html?product=${product.Id}">
-//     <img
-//       src="${product.Images.PrimaryMedium}"
-//       alt="Image of ${product.Name}"
-//     />
-//     <h3 class="card__brand">${product.Brand.Name}</h3>
-//     <h2 class="card__name">${product.NameWithoutBrand}</h2>
-//     <p class="product-card__price">$${product.FinalPrice}</p></a>
-//   </li>`;
-// }
-
-// export default async function productList(selector, category) {
-//   // get the element we will insert the list into from the selector
-//   const el = document.querySelector(selector);
-//   // get the list of products
-//   const products = await getProductsByCategory(category);
-//   console.log(products);
-//   // render out the product list to the element
-//   renderListWithTemplate(productCardTemplate, el, products);
-//   document.querySelector(".title").innerHTML = category;
-// }
